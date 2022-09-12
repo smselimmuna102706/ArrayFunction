e46540ef42716cfcde8290ca02d733bf1e690986
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-12 15:06:31 +0600

    Array

---
+++ 26.js
@@ -0,0 +1,18 @@
+
+
+var result = ["Anisul","Ripon","Akash","Furad"];
+
+document.write(result+"</br>");
+document.write(result.length+"</br>");
+
+// push method +"</br>"
+result.push("Karim");
+document.write(result+"</br>");
+document.write(result.length+"</br>");
+
+// pop method 
+
+result.pop();
+document.write(result+"</br>");
+document.write(result.length+"</br>");
+
